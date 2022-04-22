24a3e37672032fd400fd4b5b19f4ab1729c4de4a
bravint <bravint@gmail.com>
2022-04-22 20:38:52 +0100

    add temp links to football project

--- src/components/projects/Football.jsx
+++ src/components/projects/Football.jsx
@@ -6,6 +6,14 @@ import FootballPng from '../../assets/football.png';
 import '../../styles/projects.css';
 
 export const Football = () => {
+    const handleCodeClick = () => {
+        window.open('https://github.com/bravint/football-stats', '_blank');
+    }
+
+    const handleDemoClick = () => {
+        window.open('https://football-stats-bravint.vercel.app/', '_blank');
+    }
+
     return (
         <li className="project-container">
             <Parallax speed={-20}>
@@ -19,7 +27,7 @@ export const Football = () => {
                         A place for me to follow european teams, so I
                         have an overview of their form prior to matches in UEFA competitions.
                     </p>
-                    <ExternalLinks />
+                    <ExternalLinks handleCodeClick={handleCodeClick} handleDemoClick={handleDemoClick}/>
                 </div>
             </Parallax>
             <Parallax speed={20}>
