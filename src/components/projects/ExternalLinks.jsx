0d44e949632c42abc6fae457ff67b91f960ea5ee
bravint <bravint@gmail.com>
2022-04-22 12:13:37 +0100

    edit external links

--- src/components/projects/ExternalLinks.jsx
+++ src/components/projects/ExternalLinks.jsx
@@ -3,7 +3,7 @@ import '../../styles/projects.css';
 export const ExternalLinks = () => {
     return (
         <section className="external-links">
-            <p className="project-ext-links">Github Repository</p>
+            <p className="project-ext-links">View Code</p>
             <p className="project-ext-links">Live Demo</p>
         </section>
     );
